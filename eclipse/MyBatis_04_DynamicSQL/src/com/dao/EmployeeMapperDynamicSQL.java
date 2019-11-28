42670312ad35a93bb6078cb86db37d89b47187b7
bluetie915 <1402711057@qq.com>
2019-11-28 14:06:08 +0800

    判断语句

--- eclipse/MyBatis_04_DynamicSQL/src/com/dao/EmployeeMapperDynamicSQL.java
+++ eclipse/MyBatis_04_DynamicSQL/src/com/dao/EmployeeMapperDynamicSQL.java
@@ -1,5 +1,11 @@
 package com.dao;
 
+import java.util.List;
+
+import com.bean.Employee;
+
 public interface EmployeeMapperDynamicSQL {
 	
+	//携带了哪个字段就带上这个字段的值
+	public List<Employee> getEmpsByConditionIf(Employee employee);
 }
---
+++ eclipse/MyBatis_04_DynamicSQL/src/com/test/MyBatisTest3.java
@@ -0,0 +1,26 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+public class MyBatisTest3 {
+
+	public static SqlSessionFactory getFactory() throws IOException {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		return sessionFactory;
+	}
+	
+	@Test
+	public void testDynamicSql() throws IOException{
+		SqlSessionFactory factory = getFactory();
+	}
+}
