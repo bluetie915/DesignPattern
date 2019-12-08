4b998db358a4b1b526ec081ac7433707352a3110
bluetie915 <1402711057@qq.com>
2019-12-08 21:14:48 +0800

    单例模式

--- eclipse/DesignPattern/src/com/factory/simplefactory/order/PizzaStore.java
+++
@@ -1,7 +0,0 @@
-package com.factory.simplefactory.order;
-
-public class PizzaStore {
-	public static void main(String[] args) {
-		new OrderPizza();
-	}
-}
--- eclipse/DesignPattern/src/com/factory/simplefactory/order/PizzaStore3.java
+++
@@ -1,7 +0,0 @@
-package com.factory.simplefactory.order;
-
-public class PizzaStore3 {
-	public static void main(String[] args) {
-		new OrderPizza3();
-	}
-}
